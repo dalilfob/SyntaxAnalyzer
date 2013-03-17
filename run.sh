09caaf1a68db89e85916ffbaa9707b9538e3c2cc
blazindragon <theryuu@gmail.com>
2013-03-16 20:44:38 -0700

    Change bash script for UNIX

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+printf "Generating lexer from toy.flex\n"
+java -jar lib/JFlex.jar toy.flex > dump.no
 
-javac ParserDriver.java
-Java ParserDriver test1.toy
+printf "Generating parser from toy.cup\n"
+java java_cup.Main -nowarn -nosummary -expect 50 toy.cup &> dump.no
+
+printf "Compiling Driver.java\n"
+javac Driver.java
+
+printf "Usage:\n java Driver testfile\n"
